e9e0a462be48bcf7ab97fe6ba391b9fe3b273f5a
richy631 <richy631@gmail.com>
2016-03-16 11:49:50 +0800

    it can run some TA's test case

--- DB_project1.cpp
+++ DB_project1.cpp
@@ -24,7 +24,7 @@ struct Data{
 struct Table{
 	char tableName[50];
 	char* attrName[7];
-	char* types[7];//int or varchar
+	char types[7];//int(i) or varchar(v)
 	int varcharLength[7];
 	int attrNum;//how many attribute are there in this table
 	int primarykey[7];
@@ -67,29 +67,44 @@ int main(){
 
 void Analysis(char* str, char** keyword){
 	int cnt = 0;
-	char* pch;
+	char* pch = NULL;
 	// word only
 	pch = strtok (str," \n,'(){}");
 	while (pch != NULL){
-	//	printf ("%s\n",pch);
+		//printf ("%s\n",pch);
 		keyword[cnt++] = pch;
 		pch = strtok (NULL, " \n,'(){}");
 	}
 }
 
 void GetInstruction(){
+	char stack[300];
 	char* keyword[300];
+	int stackcnt = 0;
 	for(int i=0;i<300;i++){
+		stack[i] = 0;
 		keyword[i] = 0;
 	}
 	char *str = new char[300];
 	char c;
 	int cnt = 0;
 	bool isquote = 0;
-	while( (c = getchar()) != ';'){
-		//if there is '', case sensitive
-		if(c == '\'')
+	while(1){
+		c = getchar();
+		//printf("%c", c);
+		if(c == ';'){
+			str[cnt] = c;
+			break;
+		}
+		//if there is '' , case sensitive
+		else if(c == '\'')
 			isquote = !isquote;
+		else if(c == '('){
+			stack[stackcnt++] = c;
+		}
+		else if(c == ')'){
+			stackcnt--;	
+		}
 		if(isquote == 0){
 			//if there is no quote
 			if('A' <= c && c <= 'Z')
@@ -97,18 +112,23 @@ void GetInstruction(){
 		}
 		else if(c == ' ')
 			c = '_';
+			
 		str[cnt++] = c;
 	}
 	//here will print all word 
 	//printf("%s\n", str);
+	if(stackcnt!=0){
+		printf("() is not match\n"); 
+		return;
+	}
 	Analysis(str, keyword);
 	int len = 0;
 	for(int i=0;i<300;i++){
-	//	printf("keyword [%d] = %d\n", i, keyword[i]);
-	//	printf("%s\n",keyword[i]);
+		//printf("keyword [%d] = %s\n", i, keyword[i]);
+		//printf("%s\n",keyword[i]);
 		if(keyword[i] != 0){
 			len++;
-	//		printf("len = %d\n", len);
+			//printf("len = %d\n", len);
 		}
 		else break;
 	}
@@ -119,7 +139,7 @@ void GetInstruction(){
 	for(int i=0;i<len;i++){
 		printf("%s\n",keyword[i]);	
 	}
-	*/ 
+	*/
 	
 	for(int i=0;i<len;i++){
 		if(! strcmp(keyword[i], "create")){
@@ -132,12 +152,13 @@ void GetInstruction(){
 				check = check->next;
 			}
 			
-			if(sameTable) break;
-			numofTable += 1;
+			if(sameTable){
+				printf("same Table exixt!!\n");
+				break;
+			}
 			i += 2;
 			Table *newTable = new Table;
-			tail->next = newTable;
-			tail = newTable;
+			
 			//get table name
 			strcpy(newTable->tableName, keyword[i]);
 			printf("get table : %s\n", newTable->tableName);
@@ -145,33 +166,61 @@ void GetInstruction(){
 			int attrcnt = 0;
 			i += 1;
 			//now keyword[i] is attribute name
-			while(i < len){
+			int somethingWrong = 0;
+			while(i < len-1){
+				
+				//printf("i = %d, len = %d\n", i, len);
+				//printf("now keyword is = %s\n", keyword[i]);
+				if(!strcmp(keyword[i], ";"))
+					break;
+				//get the attribute and type
 				newTable->attrName[attrcnt] = keyword[i];
 				//printf("attrName[%d] get %s\n", attrcnt, newTable->attrName[attrcnt]);
 				i += 1;
-				newTable->types[attrcnt++] = keyword[i];
+				if(!strcmp(keyword[i], "varchar"))
+					newTable->types[attrcnt++] = 'v';
+				else if(!strcmp(keyword[i], "int"))
+					newTable->types[attrcnt++] = 'i';
+				else{
+					printf("Doesn't have type : %s\n", keyword[i]);
+					somethingWrong = 1;
+					break;
+				}
 				//printf("type[%d] get %s\n", attrcnt-1, newTable->types[attrcnt-1]);
-				i += 1;
 				
-				if(!strcmp(keyword[i], "primary")){
+				//printf("i = %d, len = %d\n", i, len);
+				
+				
+				//get primary key
+				if(!strcmp(keyword[i+1], "primary")){
 					newTable->primarykey[attrcnt-1] = 1;
 				//	printf("primarykey[%d] = 1\n", attrcnt-1);
-				//	printf("got primary key = %s\n", keyword[i-2]);
+				//	printf("got primary key = %s\n", keyword[i-1]);
 					i += 2;
 				}
 				//for varchar number
-				else if(isdigit(*keyword[i])){
-					stringstream s_str( keyword[i] );
+				else if(isdigit(*keyword[i+1])){
+					//printf("FIND varchar number!!\n");
+					//string to interger
+					stringstream s_str( keyword[i+1] );
 				    int num;
 				    s_str >> num;
 					newTable->varcharLength[attrcnt-1] = num;
-				//	printf("varcharLength[%d] get %d\n", attrcnt-1, num);
+					//printf("varcharLength[%d] get %d\n", attrcnt-1, num);
 					i += 1;
 				}
-				newTable->attrNum = attrcnt;
+				
+				i += 1;
+				newTable->attrNum += 1;
+				//printf("this table's attrnum = %d\n", attrcnt);
 				//printf("%d attributes !!\n\n", newTable->attrNum);
 			}
-			printf("Now have %d Table\n", numofTable);
+			if(somethingWrong == 0){
+				numofTable += 1;
+				printf("Now have %d Table\n", numofTable);
+				tail->next = newTable;
+				tail = newTable;
+			}
 		}
 		
 		else if(! strcmp(keyword[i], "insert")){
@@ -207,17 +256,15 @@ void GetInstruction(){
 						for(int k=0;k<nowtable->attrNum;k++){
 							int conflict = 0;
 							//check if it is primary key
-							//printf("nowtable's data number is %d\n", nowtable->dataCount);
-							printf("primarykey[%d] = %d\n", k, nowtable->primarykey[k]);
 							if(nowtable->primarykey[k] == 1){
 								//is primary key
 								Data *check = new Data;
 								check = root->next->dataRoot;
-								printf("got primary key!!\n\n");
+								//printf("got primary key!!\n\n");
 								for(int l=0;l<nowtable->dataCount-1;l++){
-									printf("I am in loop %d\n", l+1);
-									printf("new data is %s\n", keyword[i]);
-									printf("data in table is %s\n", check->attr[k]);
+								//	printf("I am in loop %d\n", l+1);
+								//	printf("new data is %s\n", keyword[i]);
+								//	printf("data in table is %s\n", check->attr[k]);
 									if(!strcmp(check->attr[k], keyword[i])){
 										
 										conflict = 1;
@@ -261,16 +308,16 @@ void GetInstruction(){
 							int conflict = 0;
 							//check if it is primary key
 							//printf("nowtable's data number is %d\n", nowtable->dataCount);
-							printf("primarykey[%d] = %d\n", k, nowtable->primarykey[k]);
+							//printf("primarykey[%d] = %d\n", k, nowtable->primarykey[k]);
 							if(nowtable->primarykey[k] == 1){
 								//is primary key
 								Data *check = new Data;
 								check = root->next->dataRoot;
-								printf("got primary key!!\n\n");
+								//printf("got primary key!!\n\n");
 								for(int l=0;l<nowtable->dataCount-1;l++){
-									printf("I am in loop %d\n", l+1);
-									printf("new data is %s\n", keyword[i]);
-									printf("data in table is %s\n", check->attr[k]);
+								//	printf("I am in loop %d\n", l+1);
+								//	printf("new data is %s\n", keyword[i]);
+								//	printf("data in table is %s\n", check->attr[k]);
 									if(!strcmp(check->attr[k], keyword[i])){
 										
 										conflict = 1;
@@ -312,12 +359,15 @@ void GetInstruction(){
 				for(int j=0;j<nowtable->attrNum;j++){
 				//	printf("attribute name : %s\n", nowtable->attrName[j]);
 				//	printf("type name : %s\n", nowtable->types[j]);
-					printf("%25s", nowtable->attrName[j]);
+														
+					printf("%15s", nowtable->attrName[j]);
+					if(nowtable->primarykey[j] == 1)
+						printf("#");												
 				}
 				printf("\n");
 				for(int j=0;j<nowtable->dataCount;j++){
 					for(int l=0;l<nowtable->attrNum;l++){
-						printf("%25s", nowdata->attr[l]);
+						printf("%15s", nowdata->attr[l]);
 					}
 					printf("\n");
 					nowdata = nowdata->next;
